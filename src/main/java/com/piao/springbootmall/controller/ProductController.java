9dce2d04f7dbfee2fde838ab60aefbfb521b48cf
PiaoRoger <piao9851009@gmail.com>
2024-12-29 00:22:34 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/piao/springbootmall/controller/ProductController.java
+++ src/main/java/com/piao/springbootmall/controller/ProductController.java
@@ -6,14 +6,20 @@ import com.piao.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> products = productService.getProducts();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
     @GetMapping("/products/{productId}")
     public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
         Product product = productService.getProductById(productId);
--- src/main/java/com/piao/springbootmall/service/ProductService.java
+++ src/main/java/com/piao/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.piao.springbootmall.service;
 import com.piao.springbootmall.dto.ProductRequest;
 import com.piao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
