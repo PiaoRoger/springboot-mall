fc6ca9099363bd5f09ff5174bedde16e62221a70
PiaoRoger <piao9851009@gmail.com>
2025-02-05 23:56:48 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/piao/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/piao/springbootmall/service/impl/UserServiceImpl.java
@@ -4,16 +4,32 @@ import com.piao.springbootmall.dao.UserDao;
 import com.piao.springbootmall.dto.UserRegisterRequest;
 import com.piao.springbootmall.model.User;
 import com.piao.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        if (user != null)
+        {
+            log.warn("該 email {} 已經被註冊過", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
